d8fb3660e78a6a44deaff7784c3c589835376f35
superwhat1 <david.foubertl@gmail.com>
2015-05-13 12:13:03 -0400

    FIRST 13MAY15

--- techrev/config/routes.rb
+++ techrev/config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  scope 'api' do
+    scope 'v1' do 
+
+      resources :users
+
+   end
+  end
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
