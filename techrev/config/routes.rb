19a631717f9baf8ef0888735e0dc336a4bb89a04
superwhat1 <david.foubertl@gmail.com>
2015-05-23 09:43:31 -0400

    FIRST 23MAY15

--- techrev/config/routes.rb
+++ techrev/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   namespace :api, constraints: { subdomain: 'api' }, path: 'welcome#index' do
-
+    
+    scope module: :v1 do
       resources :users
-      
+    end
+    
   end
 
 end
