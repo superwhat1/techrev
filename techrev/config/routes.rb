07449fb676388730fb41306d81ddc9036a297ca1
superwhat1 <david.foubertl@gmail.com>
2015-06-05 20:20:47 -0400

    END 05JUN15

--- techrev/config/routes.rb
+++ techrev/config/routes.rb
@@ -1,10 +1,14 @@
-Rails.application.routes.draw do
 
-  namespace :api do 
+require 'api_constraints'
+Techrev::Application.routes.draw do
+
+  namespace :api, constraints: {subdomain: 'api'}, path: '/' do
+  	scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 
       root to: 'welcome#index'
-      resources :users
-    
+      resources :users, :only => [:show, :create, :update, :destroy]
+
+    end
   end
  
 end
